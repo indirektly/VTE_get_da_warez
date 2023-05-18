08af24091b7c88b67bad3d45e1d5b350d3ba31eb
Josh <LakotaJosh@gmail.com>
2023-05-17 21:07:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def main(args):
     if(args.api == "creds.txt"):
         with open("samples/vt_creds.txt", "r") as c:
             args.api = c.readline()
+        c.close()
 
     VT_results = searchVTandDownload(args.api, args.search, args.directory)
 
@@ -36,21 +37,41 @@ def main(args):
 
 
 
-def searchVTandDownload(search_statement):
-    vt_client = vt.Client()
+def searchVTandDownload(api_key, search_statement):
+    vt_client = vt.Client(api_key)
     print("a")
+
+
 
     # Test search: p%253A5%2520type%253Adocument%2520s%253A1
     # test = c.iterator("/intelligence/search?query=p%253")
     # test4 = c.get_json("/intelligence/search?query=p%253")
     # test4['data']
-    def searchVTE(search):
-        print("a")
-    def downloadFiles(searchResults):
+    #
+    # Data Needed: (Hashes of MD5 and SHA1), (File_type), Simple Disposition, Overall Engine Disposition
+    # t["data"][i]["attributes"]["last_analysis_stats"]
+
+    VTE_JSON_results = vt_client.get_json("/intelligence/search?query=" + search_statement)
+    samples_found = {}
+    for i in range(len(VTE_JSON_results["data"])):
+        temp = {}
+        temp["md5"] = VTE_JSON_results["data"][i]["attributes"]["md5"]
+        temp["sha1"] = VTE_JSON_results["data"][i]["attributes"]["sha1"]
+        temp["sha256"] = VTE_JSON_results["data"][i]["attributes"]["sha256"]
+        if((VTE_JSON_results["data"][i]["attributes"]["last_analysis_stats"]["malicious"] > 0) or (VTE_JSON_results["data"][i]["attributes"]["last_analysis_stats"]["suspicious"])):
+            temp["Simple_Disposition"] = "malicious"
+        else:
+            temp["Simple_Disposition"] = "benign"
+        temp["Overall_Disposition"] = VTE_JSON_results["data"][i]["attributes"]["last_analysis_stats"]
+        samples_found[temp["sha256"]] = temp
+
+    
+    # client.download_file("44d88612fea8a8f36de82e1278abb02f", f)
+    def downloadFiles(samples_found):
         print("a")
 
 
-def mysteryTask(filename):
+def mysteryTask(samples_found):
     print("a")
 
 
